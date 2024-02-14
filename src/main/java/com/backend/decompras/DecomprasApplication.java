3b7bdf68c6483e49bb1c6bf3976670e98a4ba00a
Nixomart <nixoma1@gmail.com>
2024-02-14 12:37:33 -0300

    Iiniciado proyecto spring

---
+++ src/main/java/com/backend/decompras/DecomprasApplication.java
@@ -0,0 +1,13 @@
+package com.backend.decompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecomprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecomprasApplication.class, args);
+	}
+
+}
