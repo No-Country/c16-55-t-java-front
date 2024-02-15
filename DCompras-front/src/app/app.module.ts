6b6f939fb653f9755c948098ff1081463ba6d6e7
Yoel Mandelli <yoelchinomandelli@gmail.com>
2024-02-15 17:11:08 -0300

    install-angular-material

--- DCompras-front/src/app/app.module.ts
+++ DCompras-front/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DcLoginComponent } from './views/dc-login/dc-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DcLoginComponent
   ],
   imports: [
     BrowserModule,
