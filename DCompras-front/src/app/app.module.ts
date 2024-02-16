e67a16c15937164ac19e9a641c3490e8b953d8db
Yoel Mandelli <yoelchinomandelli@gmail.com>
2024-02-16 00:48:21 -0300

    add-components

--- DCompras-front/src/app/app.module.ts
+++ DCompras-front/src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DcLoginComponent } from './views/dc-login/dc-login.component';
+import { DcSidenavComponent } from './components/dc-sidenav/dc-sidenav.component';
+import { SharedModule } from 'src/shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DcLoginComponent
-  ],
+  declarations: [AppComponent, DcLoginComponent, DcSidenavComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
