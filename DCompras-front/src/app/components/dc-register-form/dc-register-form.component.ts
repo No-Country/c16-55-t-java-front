ec4b6e1be469422b0cab3b0bdb9dae8a93164b56
Marianela Cortina <marianelacort@hotmail.com>
2024-02-20 09:59:44 -0300

    Modificación Registro 2 maru

--- DCompras-front/src/app/components/dc-register-form/dc-register-form.component.ts
+++ DCompras-front/src/app/components/dc-register-form/dc-register-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dc-register-form',
@@ -11,24 +12,29 @@ export class DcRegisterFormComponent {
   formularioUsuario: FormGroup;
   
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private router: Router
   ){
     this.formularioUsuario = this.fb.group({
       nombreCompleto:['', Validators.required],
       correo:['', Validators.required],
       clave:['', Validators.required],
-      idPais:['', Validators.required],
-      idEstado:['', Validators.required],
-      idCiudad:['', Validators.required],
+      id:['', Validators.required],
+      // idEstado:['', Validators.required],
+      // idCiudad:['', Validators.required],
       direccion:['', Validators.required],
       
     });
   }
   ngOnInit() {
-    
+    this.SaveData();
+
     }
 
   
-    
+    SaveData(){
+      console.log(this.formularioUsuario.value);
+      this.router.navigate(["register"])
+    }
     
 }
