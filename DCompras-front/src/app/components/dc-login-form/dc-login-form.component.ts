de1dc5f3a93362ab19b511b5d81e40ce191e71f9
DeLxCruz <lopezsantiago3503@gmail.com>
2024-02-22 14:05:41 -0500

    feat: :zap: Improve login with credentials functionality

--- DCompras-front/src/app/components/dc-login-form/dc-login-form.component.ts
+++ DCompras-front/src/app/components/dc-login-form/dc-login-form.component.ts
@@ -1,33 +1,57 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dc-login-form',
   templateUrl: './dc-login-form.component.html',
-  styleUrls: ['./dc-login-form.component.scss']
+  styleUrls: ['./dc-login-form.component.scss'],
 })
 export class DcLoginFormComponent implements OnInit {
   loginForm: FormGroup = new FormGroup({});
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
+      email: ['', Validators.required],
       password: ['', Validators.required],
     });
+    if(window.localStorage.getItem('token')){
+      this.router.navigate(['/home/offers']);
+    }
   }
 
   onSubmit() {
     if (this.loginForm.valid) {
-      console.log(this.loginForm.value);
+      this.http
+        .post('http://localhost:8080/auth/login', this.loginForm.value)
+        .subscribe(
+          (response: any) => {
+            console.log('Formulario enviado correctamente', response);
+            localStorage.setItem('token', response.payload.token);
+            console.log(response.payload.token);
+            this.router.navigate(['/home/offers']);
+          },
+          (error) => {
+            console.log('Error al enviar el formulario', error);
+          }
+        );
     } else {
       console.log('Formulario inválido');
     }
   }
 
   toggleAutocomplete(event: any) {
-    const fields: HTMLInputElement[] = Array.from(document.querySelectorAll('.input_field'));
+    const fields: HTMLInputElement[] = Array.from(
+      document.querySelectorAll('.input_field')
+    );
     fields.forEach((field: HTMLInputElement) => {
       field.autocomplete = event.target.checked ? 'on' : 'off';
     });
