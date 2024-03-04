64abc142809e87d0ea7cc1ed6b1931019bc39c35
Yoel Mandelli <yoel.mandelli@techforb.com>
2024-03-04 16:34:28 -0300

    filter-category

--- DCompras-front/src/app/services/dc-offers.service.ts
+++ DCompras-front/src/app/services/dc-offers.service.ts
@@ -19,19 +19,16 @@ interface Product {
 export class DcOffersService {
   constructor(private http: HttpClient) {}
 
-  getAllOffers(): Observable<IDcProduct[]> {
-    return this.http.get<IDcProduct[]>('assets/db/dc-list-offers.json');
-  }
-  offers1(): Observable<any> {
-    return this.http.get<any>(
-      'https://d3e6htiiul5ek9.cloudfront.net/prod/productos?&id_categoria=02&array_sucursales=10-3-785,10-3-768,9-2-444,10-3-720,10-3-648,10-3-770,16-2-4104,10-3-765,9-1-440,10-3-769,9-2-435,2002-1-81,10-3-610,16-2-5204,10-3-772,10-3-793,10-3-607,9-2-441,10-3-790,10-3-776,10-3-642,9-1-476,10-3-641,16-2-5404,2002-1-82,10-3-606,9-1-434,10-3-764,16-2-4804,9-2-439&offset=0&limit=88&sort=-cant_sucursales_disponible'
-    );
+  searchProducts(idsSucursales: string[], categoryId: string): Observable<any> {
+    const url = `https://d3e6htiiul5ek9.cloudfront.net/prod/productos?&id_categoria=${categoryId}&array_sucursales=${idsSucursales.join(
+      ','
+    )}&offset=80&limit=99&sort=-cant_sucursales_disponible`;
+    return this.http.get<any>(url);
   }
 
   getImg(): Observable<any> {
     return this.http.get<any>('assets/db/dc-imgProduct.json');
   }
-
   getProvinceFromLocalStorage(): Observable<any[]> {
     const userInfoString = localStorage.getItem('userInfo');
     if (userInfoString !== null) {
@@ -48,29 +45,7 @@ export class DcOffersService {
     const apiUrl = `https://d3e6htiiul5ek9.cloudfront.net/prod/productos?&id_categoria=01&array_sucursales=${idsString}&offset=80&limit=99&sort=-cant_sucursales_disponible`;
     return this.http.get(apiUrl);
   }
-  buscarProductos(idsSucursales: string[]): Observable<any> {
-    const url = `https://d3e6htiiul5ek9.cloudfront.net/prod/productos?&id_categoria=01&array_sucursales=${idsSucursales.join(
-      ','
-    )}&offset=80&limit=99&sort=-cant_sucursales_disponible`;
-    return this.http.get<any>(url);
+  getCategories(): Observable<any> {
+    return this.http.get<any>('assets/db/dc-category.json');
   }
-
-  /*   offersFilter(): Observable<any> {
-    const userInfoLogueado: any = window.localStorage.getItem('userInfo');
-    const userInfo = JSON.parse(userInfoLogueado);
-
-    switch (userInfo.province) {
-      case 'Cordoba':
-        return this.http.get<any>(
-          'https://d3e6htiiul5ek9.cloudfront.net/prod/productos?&id_categoria=02&array_sucursales=10-3-785,10-3-768,9-2-444,10-3-720,10-3-648,10-3-770,16-2-4104,10-3-765,9-1-440,10-3-769,9-2-435,2002-1-81,10-3-610,16-2-5204,10-3-772,10-3-793,10-3-607,9-2-441,10-3-790,10-3-776,10-3-642,9-1-476,10-3-641,16-2-5404,2002-1-82,10-3-606,9-1-434,10-3-764,16-2-4804,9-2-439&offset=80&limit=99&sort=-cant_sucursales_disponible'
-        );
-
-      default:
-        break;
-    }
-
-    return this.http.get<any>(
-      'https://d3e6htiiul5ek9.cloudfront.net/prod/productos?&id_categoria=02&array_sucursales=10-3-785,10-3-768,9-2-444,10-3-720,10-3-648,10-3-770,16-2-4104,10-3-765,9-1-440,10-3-769,9-2-435,2002-1-81,10-3-610,16-2-5204,10-3-772,10-3-793,10-3-607,9-2-441,10-3-790,10-3-776,10-3-642,9-1-476,10-3-641,16-2-5404,2002-1-82,10-3-606,9-1-434,10-3-764,16-2-4804,9-2-439&offset=80&limit=99&sort=-cant_sucursales_disponible'
-    );
-  } */
 }
--- DCompras-front/src/app/views/dc-offers/dc-offers.component.ts
+++ DCompras-front/src/app/views/dc-offers/dc-offers.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { IAllProducts } from 'src/app/interfaces/IAllProducts';
 import { DcOffersService } from 'src/app/services/dc-offers.service';
 interface OfferResponse {
@@ -12,52 +13,52 @@ interface OfferResponse {
 export class DcOffersComponent {
   products!: any;
   province!: any;
-
-  infoProduct!: any;
-  productosLocal: any;
-  ofertas: any[] = [];
-
-  constructor(private dcOffersService: DcOffersService) {}
+  categories!: any;
+  myOffers!: FormGroup;
+  constructor(
+    private dcOffersService: DcOffersService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-    /*  this.dcOffersService.getProductosLocal().subscribe((res) => {
-      this.productosLocal = res;
-      console.log('holaaa', this.productosLocal);
-    }); */
-
-    /* 
-    this.getInfoProduct(); */
+    this.myOffers = this.fb.group({
+      category: { value: '01', disabled: false },
+    });
     this.getShopProvince();
+    this.getCategory();
   }
-  /*   getOffers() {
-    this.dcOffersService.getAllOffers().subscribe({
-      next: (response) => {
-        this.products = response;
-      },
-      error: (err) => {
-        console.log(err);
-      },
+  getShopProvince() {
+    this.dcOffersService.getProvinceFromLocalStorage().subscribe((res: any) => {
+      if (res && res.sucursales && Array.isArray(res.sucursales)) {
+        const sucursales = res.sucursales;
+        const idsSucursales: string[] = [];
+        sucursales.forEach((sucursal: any) => {
+          if (sucursal && sucursal.id) {
+            idsSucursales.push(sucursal.id);
+          }
+        });
+        const categoryControl = this.myOffers.get('category');
+        if (categoryControl) {
+          categoryControl.valueChanges.subscribe((categoryId) => {
+            this.getProduct(idsSucursales, categoryId);
+          });
+        } else {
+          console.log('El control "category" no está definido o es nulo.');
+        }
+        this.getProduct(idsSucursales, '01');
+        console.log('IDs de sucursales:', idsSucursales);
+      } else {
+        console.log(
+          'La respuesta no contiene la propiedad "sucursales" o no es un array.'
+        );
+      }
     });
-  } */
-
-  /*  getComercios() {
-    this.dcOffersService.obtenerOfertas().subscribe((data) => {
-      this.ofertas = data.items;
-    });
-  } */
-
-  /*   getProduct() {
-    this.dcOffersService.offers1().subscribe((res) => {
-      this.products = res.productos;
-      console.log(this.products);
-    });
-  } */
-
-  getProduct(idsSucursales: string[]): void {
+  }
+  getProduct(idsSucursales: string[], categoryId: string): void {
     this.dcOffersService.getImg().subscribe((res: any[]) => {
       if (res && res.length > 0) {
         this.dcOffersService
-          .buscarProductos(idsSucursales)
+          .searchProducts(idsSucursales, categoryId)
           .subscribe((response) => {
             if (response && response.productos) {
               this.products = response.productos.map((product: any) => {
@@ -67,7 +68,7 @@ export class DcOffersComponent {
                 if (matchingProduct) {
                   product.title = matchingProduct.imgUrl;
                 } else {
-                  product.title = null; // O algún otro valor por defecto si no quieres null
+                  product.title = null;
                 }
                 return product;
               });
@@ -76,65 +77,21 @@ export class DcOffersComponent {
       }
     });
   }
+  onCategorySelect(event: any) {
+    const categoryControl = this.myOffers.get('category');
 
-  /*   getInfoProduct() {
-    this.dcOffersService.offersFilter().subscribe((res) => {
-      if (res && res.productos) {
-        // Mapear los productos para mostrar solo el id, nombre y marca
-        this.infoProduct = res.productos.map((product: any) => ({
-          id: product.id,
-          nombre: product.nombre,
-          marca: product.marca,
-        }));
-        console.log('INFO', this.infoProduct);
-      }
-    });
-  } */
-  getShopProvince() {
-    this.dcOffersService.getProvinceFromLocalStorage().subscribe((res: any) => {
-      // Verificar si la respuesta contiene la propiedad 'sucursales'
-      if (res && res.sucursales && Array.isArray(res.sucursales)) {
-        // Obtener el array de sucursales
-        const sucursales = res.sucursales;
-
-        // Crear una lista para almacenar los IDs de sucursales
-        const idsSucursales: string[] = [];
-
-        // Recorrer cada sucursal y obtener el ID
-        sucursales.forEach((sucursal: any) => {
-          // Verificar si la sucursal tiene un ID
-          if (sucursal && sucursal.id) {
-            // Agregar el ID a la lista de IDs de sucursales
-            idsSucursales.push(sucursal.id);
-          }
-        });
-
-        this.getProduct(idsSucursales);
-
-        // Imprimir la lista de IDs de sucursales
-        console.log('IDs de sucursales:', idsSucursales);
-      } else {
-        console.log(
-          'La respuesta no contiene la propiedad "sucursales" o no es un array.'
-        );
-      }
+    if (categoryControl) {
+      categoryControl.setValue(event.value);
+      const categoryId = event.value;
+      console.log('datoss', categoryId);
+    } else {
+      console.log('El control de categoría es nulo.');
+    }
+  }
+  getCategory() {
+    this.dcOffersService.getCategories().subscribe((res) => {
+      this.categories = res;
+      console.log(this.categories);
     });
   }
-
-  buscarProductosPorSucursales(idsSucursales: string[]): void {
-    // Llamar al servicio del otro componente y pasarle los idsSucursales
-    this.dcOffersService.buscarProductos(idsSucursales).subscribe(
-      (respuesta: any) => {
-        console.log('datosss', respuesta);
-      },
-      (error: any) => {
-        console.error(
-          'Ocurrió un error al llamar al servicio del otro componente:',
-          error
-        );
-      }
-    );
-  }
-
-  getItems() {}
 }
